0b984c6c642847fd591d6d1e13e62c8d6d52ab93
YoungdonJung <yeavov@gmail.com>
2024-03-12 15:07:22 +0900

    change 21610

--- Solution/21610.cpp
+++ Solution/21610.cpp
@@ -1,155 +1,135 @@
 #include <iostream>
-#include <cstring>
-#include <algorithm>
-#include <queue>
-#include <vector>
+
 using namespace std;
 
-#define MAX 55
+#define MAX (50 + 5)
 
-int N, M, Answer;
+int N, M;
 int MAP[MAX][MAX];
-bool Cloud_MAP[MAX][MAX];
-vector<pair<int, int>> Cloud;
-vector<pair<int, int>> Cmd;
 
-int dx[] = { 0, 0, -1, -1, -1, 0, 1, 1, 1};
-int dy[] = { 0, -1, -1, 0, 1, 1, 1, 0, -1};
+int dr[] = {0, 0, -1, -1, -1, 0, 1, 1, 1};
+int dc[] = {0, -1, -1, 0, 1, 1, 1, 0, -1};
+int drr[] = {-1, -1, 1, 1};
+int dcc[] = {-1, 1, 1, -1};
+typedef struct st{
+    int r;
+    int c;
+}RC;
 
-void Input(){
+RC cloud[MAX * MAX] = { 0 };
+int ccnt;
+
+void input(){
     cin >> N >> M;
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            cin >> MAP[i][j];
-        }
-    }
-    for(int i = 0; i < M; i++){
-        int a, b;
-        cin >> a >> b;
-        Cmd.push_back({a, b});
-    }
-}
 
-void Init_Cloud(){
-    Cloud.push_back({N - 2, 0});
-    Cloud.push_back({N - 2, 1});
-    Cloud.push_back({N - 1, 0});
-    Cloud.push_back({N - 1, 1});
-    Cloud_MAP[N - 2][0] = true;
-    Cloud_MAP[N - 2][1] = true;
-    Cloud_MAP[N - 1][0] = true;
-    Cloud_MAP[N - 1][1] = true;
-}
-
-int Make_Range(int x){
-    if(x < 0) return N - 1;
-    if(x >= N) return 0;
-    return x;
-}
-
-void Move_Cloud(int Idx){
-    int Dir = Cmd[Idx].first;
-    int Cnt = Cmd[Idx].second;
-
-    memset(Cloud_MAP, false, sizeof(Cloud_MAP));
-    for(int i = 0; i < Cloud.size(); i++){
-        int x = Cloud[i].first;
-        int y = Cloud[i].second;
-        int nx = x;
-        int ny = y;
-        for(int j = 0; j < Cnt; j++){
-            nx += dx[Dir];
-            ny += dy[Dir];
-            nx = Make_Range(nx);
-            ny = Make_Range(ny);
-        }
-        Cloud[i].first = nx;
-        Cloud[i].second = ny;
-    }
-    for(int i = 0; i < Cloud.size(); i++){
-        int x = Cloud[i].first;
-        int y = Cloud[i].second;
-        Cloud_MAP[x][y] = true;
-    }
-}
-
-void Make_Rain(){
-    for(int i = 0; i < Cloud.size(); i++){
-        int x = Cloud[i].first;
-        int y = Cloud[i].second;
-        MAP[x][y]++;
-    }
-}
-
-void Water_Bug(){
-    for(int i = 0; i < Cloud.size(); i++){
-        int x = Cloud[i].first;
-        int y = Cloud[i].second;
-        int Cnt = 0;
-        for(int j = 2; j <= 8; j += 2){
-            int nx = x + dx[j];
-            int ny = y + dy[j];
-            if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-            if(MAP[nx][ny] >= 1) Cnt++;
-        }
-        MAP[x][y] += Cnt;
-    }
-}
-
-void Delete_Cloud(){
-    Cloud.clear();
-}
-
-void Make_Cloud(){
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            if(Cloud_MAP[i][j] == true) continue;
-            if(MAP[i][j] < 2) continue;
-            MAP[i][j] -= 2;
-            Cloud.push_back({i, j});
+    for(int r = 1; r <= N; r++){
+        for(int c = 1; c <= N; c++){
+            cin >> MAP[r][c];
         }
     }
 
-    memset(Cloud_MAP, false, sizeof(Cloud_MAP));
-    for(int i = 0; i < Cloud.size(); i++){
-        int x = Cloud[i].first;
-        int y = Cloud[i].second;
-        Cloud_MAP[x][y] = true;
-    }
+    cloud[0].r = N; cloud[0].c = 1;
+    cloud[1].r = N; cloud[1].c = 2;
+    cloud[2].r = N - 1; cloud[2].c = 1;
+    cloud[3].r = N - 1; cloud[3].c = 2;
+    ccnt = 4;
 }
 
-void Find_Answer(){
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            Answer += MAP[i][j];
+void moveCloud(int dir, int size){
+    for(int i = 0; i < ccnt; i++){
+        for(int s = 0; s < size; s++){
+            int nr, nc;
+
+            nr = cloud[i].r + dr[dir];
+            nc = cloud[i].c + dc[dir];
+
+            if(nr == N + 1) nr = 1;
+            if(nr == 0) nr = N;
+            if(nc == N + 1) nc = 1;
+            if(nc == 0) nc = N;
+
+            cloud[i].r = nr;
+            cloud[i].c = nc;
         }
     }
 }
 
-void Solution(){
-    Init_Cloud();
-    for(int i = 0; i < M; i++){
-        Move_Cloud(i);
-        Make_Rain();
-        Water_Bug();
-        Delete_Cloud();
-        Make_Cloud();
+void addWater(){
+    for(int i = 0; i < ccnt; i++){
+        MAP[cloud[i].r][cloud[i].c]++;
     }
-    Find_Answer();
-    cout << Answer << "\n";
 }
 
-void Solve(){
-    Input();
-    Solution();
+void copyWater(){
+    for(int i = 0; i < ccnt; i++){
+        int countCloud = 0;
+        for(int dir = 0; dir < 4; dir++){
+            int nr, nc;
+            
+            nr = cloud[i].r + drr[dir];
+            nc = cloud[i].c + dcc[dir];
+
+            countCloud += !!MAP[nr][nc];
+        }
+
+        MAP[cloud[i].r][cloud[i].c] += countCloud;
+    }
+}
+
+int checkCloud(int r, int c){
+    for(int i  = 0; i < ccnt; i++){
+        if(cloud[i].r == r && cloud[i].c == c) return 1;
+    }
+
+    return 0;
+}
+
+void makeCloud(){
+    RC newCloud[MAX * MAX] = { 0 };
+    int ncnt = 0;
+
+    for(int r = 1; r <= N; r++){
+        for(int c = 1; c <= N; c++){
+            if(MAP[r][c] < 2) continue;
+
+            if(checkCloud(r, c)) continue;
+
+            MAP[r][c] -= 2;
+
+            newCloud[ncnt].r = r;
+            newCloud[ncnt++].c = c;
+        }
+    }
+
+    for(int i = 0; i < ncnt; i++){
+        cloud[i] = newCloud[i];
+    }
+
+    ccnt = ncnt;
+
 }
 
 int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+    input();
 
-    Solve();
+    for(int i = 0; i < M; i++){
+        int d, s;
 
+        cin >> d >> s;
+
+        moveCloud(d, s);
+        addWater();
+        copyWater();
+        makeCloud();
+    }
+
+    int sum = 0;
+    for(int r = 1; r <= N; r++){
+        for(int c = 1; c <= N; c++){
+            sum += MAP[r][c];
+        }
+    }
+
+    cout<<sum<<"\n";
     return 0;
 }

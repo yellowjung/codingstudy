a6a0d332731823d1cc6162439de83c8639afcfc5
Youngdon Jung <yeavov@gmail.com>
2024-04-05 16:09:47 +0900

    rabbit ing

--- Solution/rabbit.cpp
+++ Solution/rabbit.cpp
@@ -1,174 +1,126 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 #include <queue>
-#include <cmath>
+#include <map>
+
 using namespace std;
 
-int N, M, K;
-int Ex, Ey;
-int Map[11][11];
+#define MAX_N 2000
 
-int Ans = 0;
+int n, m, p;
 
-int dx[4] = {1, -1, 0, 0};
-int dy[4] = {0, 0, 1, -1};
+int id[MAX_N + 1];
 
-struct Person{
-    int x;
-    int y;
+int pw[MAX_N + 1];
+
+int jump_cnt[MAX_N + 1];
+
+long long result[MAX_N + 1];
+
+pair<int, int> point[MAX_N + 1];
+
+map<int, int> id_to_idx;
+
+bool is_runned[MAX_N + 1];
+
+long long total_sum;
+
+struct Rabbit{
+    int x, y, j, id;
+
+    bool operator<(const Rabbit &b) const{
+        if(j != b.j) return j >b.j;
+        if(x + y != b.x + b.y) return x + y > b.x + b.y;
+        if(x != b.x) return x > b.x;
+        if(y != b.y) return y > b.y;
+        return id > b.id;
+    }
 };
 
-struct Position{
-    int x; 
-    int y;
-    int Size;
-};
-
-vector<Person> Vector;
-vector<Position> Data;
-
-
-void input(){
-    cin >> N >> M >> K;
-    for(int i = 1; i <= N; i++){
-        for(int j = 1; j <= N; j++){
-            cin >> Map[i][j];
-        }
-    }
-
-    int X;
-    int Y;
-    for(int i = 1; i <= M; i++){
-        cin >> X >> Y;
-        Vector.push_back({X, Y});
-    }
-
-    cin >> Ex >> Ey;
+bool Cmp(Rabbit a, Rabbit b){
+    if(a.x + a.y != b.x + b.y) return a.x + a.y < b.x + b.y;
+    if(a.x != b.x) return a.x < b.x;
+    if(a.y != b.y) return a.y < b.y;
+    return a.id < b.id;
 }
 
-int Dist_Exit(int x, int y){
-    return abs(x - Ex) + abs(y - Ey);
-}
-
-void Exit(Person& P){
-    int Curr = Dist_Exit(P.x, P.y);
-
-    for(int i = 0; i < 4; i++){
-        int nx = P.x + dx[i];
-        int ny = P.y + dy[i];
-        int Next = Dist_Exit(nx, ny);
-
-        if(nx < 1 || ny < 1 || nx > N || ny > N) continue;
-        if(Curr <= Next) continue;
-        if(Map[nx][ny] > 0) continue;
-
-        Ans++;
-        P.x = nx;
-        P.y = ny;
-        break;
+void init(){
+    cin >> n >> m >> p;
+    for(int i = 1; i <= p; i++){
+        cin >> id[i] >> pw[i];
+        id_to_idx[id[i]] = i;
+        point[i] = make_pair(1, 1);
     }
 }
 
-void Square_Data(int x , int y){
-    int x_Gap = abs(x - Ex);
-    int y_Gap = abs(y - Ey);
+Rabbit GetUpRabbit(Rabbit cur_rabbit, int dis){
+    Rabbit up_rabbit = cur_rabbit;
+    dis %= 2 * (n - 1);
 
-    int Size = max(x_Gap, y_Gap);
+    if(dis >= up_rabbit.x - 1){
+        dis -= (up_rabbit.x - 1);
+        up_rabbit.x = 1;
+    }else{
+        up_rabbit.x -= dis;
+        dis = 0;
+    }
 
-    int mx = max(Ex, x) - Size;
-    if(mx < 1) mx = 1;
+    if(dis >= n - up_rabbit.x){
+        dis -= (n - up_rabbit.x);
+        up_rabbit.x = n;
+    }else{
+        up_rabbit.x += dis;
+        dis = 0;
+    }
 
-    int my = max(Ey, y) - Size;
-    if(my < 1) my = 1;
+    up_rabbit.x -= dis;
 
-    Data.push_back({mx ,my, Size});
-
+    return up_rabbit;
 }
 
-bool Cmp(Position& A, Position& B){
-    if(A.Size == B.Size){
-        if(A.x == B.x){
-            return A.y < B.y;
-        }
-        return A.x < B.x;
-    }
-    return A.Size < B.Size;
+Rabbit GetDownRabbit(Rabbit cur_rabbit, int dis){
+    Rabbit down_rabbit = cur_rabbit;
+    dis %= 2 * (n - 1);
+
+    
 }
 
-void Rotation(int x, int y, int Size){
-    vector<pair<pair<int, int>, int>> Pair;
-    bool Flag = true;
+void start_round(){
+    int k, bonus;
+    cin >> k >> bonus;
+    priority_queue <Rabbit> rabbit_pq;
 
-    for(int i = 0; i <= Size; i++){
-        for(int j = 0; j <= Size; j++){
-            int px = x + i;
-            int py = y + j;
-
-            int nx = x + j;
-            int ny = y + Size - i;
-
-            if(px == Ex && py == Ey && Flag == true){
-                Flag = false;
-                Ex = nx;
-                Ey = ny;
-            }
-            
-            Pair.push_back({{nx, ny}, Map[px][py]});
-        }
+    for(int i = 1; i <= p; i++){
+        is_runned[i] = false;
     }
 
-    for( auto E : Pair){
-        int x = E.first.first;
-        int y = E.first.second;
-        int m = E.second;
-        if(m == 0){
-            Map[x][y] = 0;
-        }else if(m > 0){
-            Map[x][y] = m - 1;
-        }
+    for(int i = 1; i<= p; i++){
+        Rabbit new_rabbit;
+        new_rabbit.x = point[i].first;
+        new_rabbit.y = point[i].second;
+        new_rabbit.j = jump_cnt[i];
+        new_rabbit.id = id[i];
+
+        rabbit_pq.push(new_rabbit);
     }
 
-    for( auto& E : Vector){
-        if(E.x >= x && E.x <= x + Size){
-            if(E.y >= y && E.y <= y + Size){
-                int i = E.x - x;
-                int j = E.y - y;
 
-                E.x = x + j;
-                E.y = y + Size -i;
-            }
-        }
+    while(k--){
+        Rabbit cur_rabbit = rabbit_pq.top(); rabbit_pq.pop();
+
+        int dis = pw[id_to_idx[cur_rabbit.id]];
+        Rabbit next_rabbit = cur_rabbit;
+        next_rabbit.x = 0;
+        next_rabbit.y = 0;
+
+        Rabbit up_rabbit = GetUpRabbit(cur_rabbit, dis);
+
+        if(Cmp(next_rabbit, up_rabbit)) next_rabbit = up_rabbit;
+
+        Rabbit down_rabbit = GetDownRabbit(cur_rabbit, dis);
+
+
+
     }
-}
-
-void solve(){
-    while(K--){
-        for(int i = 0; i < Vector.size(); i++){
-            Exit(Vector[i]);
-
-            if((Vector[i].x == Ex) && (Vector[i].y == Ey)){
-                Vector.erase(Vector.begin() + i);
-                i--;
-                continue;
-            }
-
-            Square_Data(Vector[i].x, Vector[i].y);
-        }
-
-        if(Vector.size() == 0) break;
-
-        sort(Data.begin(), Data.end(), Cmp);
-        Rotation(Data[0].x, Data[0].y, Data[0].Size);
-        Data.clear();
-    }
-}
-
-void solution(){
-    input();
-    solve();
-    cout << Ans <<"\n";
-    cout<< Ex << ' '<< Ey <<"\n";
 }
 
 int main(){
@@ -176,7 +128,29 @@ int main(){
     cin.tie(0);
     cout.tie(0);
 
-    solution();
+    int q;
+    cin >> q;
+
+    while(q--){
+        int query;
+        cin >> query;
+
+        if(query == 100){
+            init();
+        }
+
+        if(query == 200){
+            start_round();
+        }
+
+        if(query == 300){
+
+        }
+
+        if(query == 400){
+
+        }
+    }
 
     return 0;
 }
